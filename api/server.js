e1bb41f3373991a6c6e5459583e78cee0b780a56
Drew Nelson <realseal177@gmail.com>
2022-02-10 19:16:54 -0800

    finally pulling

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const server = express();
 
-// const projectsRouter = require('./projects/projects-router');
+const projectsRouter = require('./projects/projects-router');
 
 server.use(express.json());
 
-// server.use('/api/projects', projectsRouter);
+server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h2>Hello!</h2>`);
