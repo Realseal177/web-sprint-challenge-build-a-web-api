3730a070c9c73745ff319799c3654b35035800cf
Drew Nelson <realseal177@gmail.com>
2022-02-10 18:59:39 -0800

    server and index built

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,12 @@
-// add middlewares here related to projects
+const { get } = require('./projects-model');
+
+function logger(req, res, next) {
+    const time = Date.now().toLocaleString();
+    const { url, method } = req;
+    console.log(`[${method}] ${url} --(${time})--`);
+    next();
+}
+
+module.exports = {
+    logger,
+};
