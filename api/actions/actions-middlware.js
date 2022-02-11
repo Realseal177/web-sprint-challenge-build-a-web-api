213b5c55771c90e22bd986e6b42df88ce3cd3ff4
Drew Nelson <realseal177@gmail.com>
2022-02-11 10:36:58 -0800

    all tests passing

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,44 @@
-// add middlewares here related to actions
+const { get } = require('./actions-model');
+
+function logger(req, res, next) {
+    const time = Date.now().toLocaleString();
+    const { url, method } = req;
+    console.log(`[${method}] ${url} --(${time})--`);
+    next();
+}
+
+function validateActionById(req, res, next) {
+    get(req.params.id) 
+        .then(action => {
+            if(action) {
+                req.action = action;
+                next();
+            } else {
+                next({
+                    status: 404,
+                    message: 'action not found'
+                })
+            }
+        })
+        .catch(next)
+}
+
+function validateAction(req, res, next) {
+    const { project_id, description, notes } = req.body;
+    if (project_id && description && notes) {
+        req.description = description.trim();
+        req.notes = notes.trim();
+        next()
+    } else {
+        next({
+            status: 400,
+            message: 'Project requires project_id, description, and notes',
+        })
+    }
+}
+
+module.exports = {
+    logger,
+    validateActionById,
+    validateAction,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,58 @@
-// Write your "actions" router here!
+const router = require('express').Router();
+const Action = require('./actions-model');
+const {
+    logger,
+    validateActionById,
+    validateAction,
+} = require('./actions-middlware');
+
+router.get('/', (req, res, next) => {
+    Action.get()
+      .then(actions => {
+        console.log(actions);
+        res.status(200).json(actions);
+      })
+      .catch(next)
+  });
+
+router.get('/:id', validateActionById, (req, res) => {
+    res.json(req.action)
+});
+
+router.post('/', validateAction, (req, res, next) => {
+    Action.insert(req.body)
+        .then(actions => {
+            res.status(201).json({
+                ...actions,
+                id: Number(req.params.id)
+            });
+        })
+        .catch(next)
+});
+
+router.put('/:id', validateActionById, validateAction, (req, res, next) => {
+    if(req.body.completed || req.body.completed === false) {
+        Action.update(req.params.id, req.body)
+            .then(actions => {
+                res.status(200).json(actions)
+            })
+            .catch(next);
+        } else {
+            next({
+                status: 400,
+                message: 'Action requires name and description'
+            })
+        }
+});
+
+router.delete('/:id', validateActionById, (req, res, next) => {
+    Action.remove(req.params.id)
+        .then(() => {
+            res.json(req.action)
+        })
+        .catch(next);
+});
+
+router.use(logger);
+
+module.exports = router;
